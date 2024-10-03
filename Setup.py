cd2c1032234d1a1e6e13fba5366476cd9605d084
Crimson <156434805+Egoistsm@users.noreply.github.com>
2024-10-04 01:09:57 +0700

    Update Setup.py

--- Setup.py
+++ Setup.py
@@ -4,5 +4,5 @@ setup(
     name="Synapse X Testing",
     version="0.1",
     description="DupeOS is roblox executor 100%UNC testing pre release.",
-    executables=[Executable("Synapse X.py")],
+    executables=[Executable("Bomba.py")],
 )
